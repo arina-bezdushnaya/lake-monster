275aeb79e862b9f8ba98f8570889e2805d994e56
Arina Bezdushnaia <arina.bezdushnaia@stm-labs.ru>
2024-04-17 23:06:11 +0300

    * prettier * change lastScore

--- src/features/score-table/ScoreTable.tsx
+++ src/features/score-table/ScoreTable.tsx
@@ -1,67 +1,68 @@
-import React from "react";
-import { Table } from "antd";
-import { mockData, TableRow } from "./mock-data";
-import { Medal, NoMedal } from "./styled";
-
+import React from 'react';
+import {Table} from 'antd';
+import {mockData, TableRow} from './mock-data';
+import {Medal, NoMedal} from './styled';
 
 export const ScoreTable = () => {
   const columns = [
     {
-      title: "No",
-      dataIndex: "no",
-      key: "no",
+      title: 'No',
+      dataIndex: 'no',
+      key: 'no',
       width: 60,
       render: (value: number) => {
-        return (
-          value > 3 ?
-            <NoMedal>{value}</NoMedal> :
-            <Medal value={value}>{value}</Medal>
+        return value > 3 ? (
+          <NoMedal>{value}</NoMedal>
+        ) : (
+          <Medal value={value}>{value}</Medal>
         );
-      }
+      },
     },
     {
-      title: "Name",
-      dataIndex: "name",
-      key: "name",
-      width: 300
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+      width: 300,
     },
     {
-      title: "Ratio",
-      dataIndex: "ratio",
-      key: "ratio",
+      title: 'Ratio',
+      dataIndex: 'ratio',
+      key: 'ratio',
       sorter: (a: TableRow, b: TableRow) => a.ratio - b.ratio,
-      width: 200
+      width: 200,
     },
     {
-      title: "Clue",
-      dataIndex: "clue",
-      key: "clue",
-      render: (value: boolean) => value ? "Yes" : "No"
-    }
+      title: 'Clue',
+      dataIndex: 'clue',
+      key: 'clue',
+      render: (value: boolean) => (value ? 'Yes' : 'No'),
+    },
   ];
 
-  const personalScore = localStorage.getItem("lake_monster");
-  let lastScore = JSON.parse(personalScore || "{}");
-  lastScore = personalScore ? lastScore : [];
+  const personalScore = localStorage.getItem('lake_monster');
+  let lastScore = JSON.parse(personalScore || '{}');
+  lastScore = personalScore ? [lastScore] : [];
 
   const dataSource = [...mockData, ...lastScore].map((row, index) => {
-    return ({ ...row, no: index + 1, key: index + 1 });
+    return {...row, no: index + 1, key: index + 1};
   });
 
   const getData = (current: number, pageSize: number) => {
     return dataSource.slice((current - 1) * pageSize, current * pageSize);
   };
 
-  return <>
-    <Table
-      dataSource={dataSource}
-      columns={columns}
-      pagination={{
-        defaultPageSize: 5,
-        showSizeChanger: true,
-        pageSizeOptions: ["2", "5"],
-        onChange: (page: number, pageSize: number) => getData(page, pageSize)
-      }}
-    />
-  </>;
+  return (
+    <>
+      <Table
+        dataSource={dataSource}
+        columns={columns}
+        pagination={{
+          defaultPageSize: 5,
+          showSizeChanger: true,
+          pageSizeOptions: ['2', '5'],
+          onChange: (page: number, pageSize: number) => getData(page, pageSize),
+        }}
+      />
+    </>
+  );
 };
